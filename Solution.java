df8fcea17db112eed35e44f0a1085229bd7319ce
kamran6433 <kamran_12@icloud.com>
2022-06-21 15:07:32 +0100

    Update.

---
+++ Solution.java
@@ -0,0 +1,12 @@
+/**
+ * This is my solution class where I will be getting my answer and
+ * running the program
+ */
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
